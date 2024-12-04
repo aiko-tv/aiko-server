b1bc2f4c9a1e2b0c053230394f8a91f4f3b47661
0xNerd <f00xtenji@gmail.com>
2024-12-04 15:17:54 -0500

    uses pfp and handle from users schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const CommentSchema = new mongoose.Schema({
   id: {
     type: String,
@@ -21,9 +22,9 @@ const CommentSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   },
-  avatar: { // Add avatar field
+  avatar: { // This field can now be ignored, as it will be overwritten
     type: String,
-    required: true
+    required: false
   },
   handle: { // Add handle field
     type: String,
@@ -34,5 +35,28 @@ const CommentSchema = new mongoose.Schema({
     default: false
   }
 });
+
+// Virtual population for user profile data
+CommentSchema.virtual('userProfile', {
+  ref: 'UserProfile',
+  localField: 'user', // This refers to the publicKey in Comment
+  foreignField: 'publicKey', // This is the publicKey in UserProfile
+  justOne: true, // Only one user profile per comment
+});
+
+CommentSchema.set('toObject', { virtuals: true });
+CommentSchema.set('toJSON', { virtuals: true });
+
+// Add a custom method to overwrite avatar and handle from UserProfile
+CommentSchema.methods.setAvatarAndHandleFromUserProfile = async function() {
+  if (this.userProfile) {
+    if (this.userProfile.pfp) {
+      this.avatar = this.userProfile.pfp;  // Set the avatar to pfp
+    }
+    if (this.userProfile.handle) {
+      this.handle = this.userProfile.handle; // Set the handle from userProfile
+    }
+  }
+};
 
 module.exports = mongoose.model('Comment', CommentSchema);
